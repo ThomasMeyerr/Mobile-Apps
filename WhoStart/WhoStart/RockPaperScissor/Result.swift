3c5db9157ad26eb77a78e5ec3a72c13dd4d5076f
Modzie <modzielol@gmail.com>
2024-01-17 14:16:48 +0100

    Add: after Result() simulate the back button instead of generate a new view

--- WhoStart/WhoStart/RockPaperScissor/Result.swift
+++ WhoStart/WhoStart/RockPaperScissor/Result.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct Result: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     let yours: String
     let AI: String
 
@@ -15,7 +17,9 @@ struct Result: View {
         ZStack {
             if yours == "Rock" && AI == "Scissor" || yours == "Paper" && AI == "Rock" || yours == "Scissor" && AI == "Paper" {
                 RadialGradient(colors: [.green, .secondary], center: .center, startRadius: 20, endRadius: 500)
-                NavigationLink(destination: RockPaperScissors()) {
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
                     Image("Win")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
@@ -23,7 +27,9 @@ struct Result: View {
                 }
             } else if yours == "Rock" && AI == "Paper" || yours == "Paper" && AI == "Scissor" || yours == "Scissor" && AI == "Rock" {
                 RadialGradient(colors: [.secondary, .white], center: .center, startRadius: 20, endRadius: 500)
-                NavigationLink(destination: RockPaperScissors()) {
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
                     Image("Loose")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
@@ -31,7 +37,9 @@ struct Result: View {
                 }
             } else {
                 RadialGradient(colors: [.secondary, .white], center: .center, startRadius: 20, endRadius: 500)
-                NavigationLink(destination: RockPaperScissors()) {
+                Button(action: {
+                    self.presentationMode.wrappedValue.dismiss()
+                }) {
                     Image("Par")
                         .resizable()
                         .aspectRatio(contentMode: .fit)
