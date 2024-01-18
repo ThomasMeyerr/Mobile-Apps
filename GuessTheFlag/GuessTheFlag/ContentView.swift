ce3a088594aa241798d067a7ff122fb1234c19f5
Modzie <modzielol@gmail.com>
2024-01-18 10:00:27 +0100

    Add: alert to see if it's the correct answer and display a new quizz when press continue

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -9,6 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var showingScore = false
+    @State private var scoreTitle = String()
+    
     var body: some View {
         ZStack {
             Color.blue.ignoresSafeArea()
@@ -18,19 +23,39 @@ struct ContentView: View {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
                     
-                    Text(countries[correctAnswer])
+                    Text(self.countries[self.correctAnswer])
                         .foregroundColor(.white)
                 }
                 
                 ForEach(0..<3) { number in
                     Button {
-                        // flag tapped
+                        self.flagTapped(number)
                     }   label: {
-                        Image(countries[number])
+                        Image(self.countries[number])
                     }
                 }
             }
         }
+        .alert(self.scoreTitle, isPresented: self.$showingScore) {
+            Button("Continue", action: askQuestion)
+        } message : {
+            Text("Your score is ???")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == self.correctAnswer {
+            self.scoreTitle = "Correct"
+        } else {
+            self.scoreTitle = "Wrong"
+        }
+        
+        self.showingScore = true
+    }
+    
+    func askQuestion() {
+        self.countries.shuffle()
+        self.correctAnswer = Int.random(in: 0...2)
     }
 }
 
