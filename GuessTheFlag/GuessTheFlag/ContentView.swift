7caf42cbc8eae12c6234d8f70bd8d045c92cd187
Modzie <modzielol@gmail.com>
2024-01-18 10:46:52 +0100

    Refactor: Old skin is better

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -16,10 +16,7 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            RadialGradient(stops: [
-                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
-                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
-            ], center: .top, startRadius: 200, endRadius: 400)
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea()
             
             VStack {
@@ -36,6 +33,7 @@ struct ContentView: View {
                             .font(.subheadline.weight(.heavy))
                         
                         Text(self.countries[self.correctAnswer])
+                            .foregroundStyle(.secondary)
                             .font(.largeTitle.weight(.semibold))
                     }
                     
@@ -53,7 +51,7 @@ struct ContentView: View {
                     Spacer()
                     
                     Text("Score ???")
-                        .foregroundColor(.white)
+                        .foregroundStyle(.secondary)
                         .font(.title.bold())
                     
                     Spacer()
