d72b08ff0ed0b67011c8a3e9c867074d2eb3864c
Modzie <modzielol@gmail.com>
2024-01-18 09:39:58 +0100

    Add: Data file to handle global data for app

---
+++ GuessTheFlag/GuessTheFlag/Data.swift
@@ -0,0 +1,9 @@
+//
+//  Data.swift
+//  GuessTheFlag
+//
+//  Created by Thomas Meyer on 18/01/2024.
+//
+
+let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+var correctAnswer = Int.random(in: 0...2)
