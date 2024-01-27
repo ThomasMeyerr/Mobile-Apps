22c72fea4444cd0c9628e477278496999595da9c
Modzie <modzielol@gmail.com>
2024-01-27 14:18:03 +0100

    Add: placeholder for launching screen for Ranked

--- GuessTheFlag/GuessTheFlag/Ranked/Ranked.swift
+++ GuessTheFlag/GuessTheFlag/Ranked/Ranked.swift
@@ -8,14 +8,27 @@
 import SwiftUI
 
 struct Ranked: View {
+    
+    @State private var showingGame = false
+    
     var body: some View {
-        ZStack {
-            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom)
-                .ignoresSafeArea()
-            
-            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .foregroundColor(.white)
+        NavigationStack {
+            ZStack {
+                LinearGradient(colors: [Color(red: 52/255, green: 103/255, blue: 51/255), .black], startPoint: .top, endPoint: .bottom)
+                    .ignoresSafeArea()
+                
+                NavigationLink(destination: Game(countries: world), isActive: self.$showingGame) {
+                    EmptyView()
+                }
+                
+                Button(action: {
+                    self.showingGame = true
+                }) {
+                    Label("START", systemImage: "globe")
+                }
+            }
         }
+        .accentColor(.white)
     }
 }
 
