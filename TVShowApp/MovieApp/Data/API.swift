f8e10b9af9977545828deba37abc7d0a6c152a6c
Thomas Meyer <thmeyer@student.42lyon.fr>
2024-02-15 11:14:49 +0100

    Add: API file to stock the main class

---
+++ TVShowApp/MovieApp/Data/API.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  MovieApp
+//
+//  Created by Thomas Meyer on 15/02/2024.
+//
+
+import SwiftUI
+
+/* ------- MAIN API CLASS ------- */
+class TVMazeAPI {
+    static let shared = TVMazeAPI()
+    private init() {}
+
+    func searchShows(query: String, completion: @escaping (Result<[Show], Error>) -> Void) {
+        let urlString = "https://api.tvmaze.com/search/shows?q=\(query)"
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NetworkError.invalidURL))
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else {
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            do {
+                let searchResults = try JSONDecoder().decode([SearchResult].self, from: data)
+                let shows = searchResults.map { $0.show }
+                completion(.success(shows))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
--- TVShowApp/MovieApp/Data/Data.swift
+++ TVShowApp/MovieApp/Data/Data.swift
@@ -7,41 +7,14 @@
 
 import SwiftUI
 
+/* ------- ERROR FOR API ------- */
 enum NetworkError: Error {
     case invalidURL
     case noData
 }
 
-class TVMazeAPI {
-    static let shared = TVMazeAPI()
-    private init() {}
 
-    func searchShows(query: String, completion: @escaping (Result<[Show], Error>) -> Void) {
-        let urlString = "https://api.tvmaze.com/search/shows?q=\(query)"
-        guard let url = URL(string: urlString) else {
-            completion(.failure(NetworkError.invalidURL))
-            return
-        }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(.failure(error))
-                return
-            }
-            guard let data = data else {
-                completion(.failure(NetworkError.noData))
-                return
-            }
-            do {
-                let searchResults = try JSONDecoder().decode([SearchResult].self, from: data)
-                let shows = searchResults.map { $0.show }
-                completion(.success(shows))
-            } catch {
-                completion(.failure(error))
-            }
-        }.resume()
-    }
-}
-
+/* ------- STRUCTS FOR JSON ------- */
 struct SearchResult: Codable {
     let show: Show
 }
