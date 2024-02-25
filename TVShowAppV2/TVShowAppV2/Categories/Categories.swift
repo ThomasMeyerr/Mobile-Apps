64552052d2b53f03accd811425bec4134b981581
Modzie <modzielol@gmail.com>
2024-02-25 17:58:54 +0100

    Add: Protection for tabitems in Categories view, need to implant API for choosing genres

--- TVShowAppV2/TVShowAppV2/Categories/Categories.swift
+++ TVShowAppV2/TVShowAppV2/Categories/Categories.swift
@@ -9,27 +9,29 @@ import SwiftUI
 
 struct Categories: View {
     var body: some View {
-        ZStack {
-            Color.white.ignoresSafeArea()
-                        
-            VStack {
-                Spacer()
-                Spacer()
-                Text("Categories :")
-                    .foregroundColor(.white)
-                    .font(.title.bold())
-                    .background(Color.blue.frame(width: 400, height: 200).ignoresSafeArea())
+        NavigationView {
+            ZStack {
+                Color.white.ignoresSafeArea()
                 
-                List {
-                    ForEach(genres, id: \.self) { genre in
-                        HStack {
-                            Image(systemName: "tag")
-                                .foregroundColor(.blue)
-                                .imageScale(.medium)
-                            
-                            Text(genre)
-                                .foregroundColor(.blue)
-                                .font(Font.system(size: 20, weight: .bold))
+                VStack {
+                    Spacer()
+                    Spacer()
+                    Text("Categories :")
+                        .foregroundColor(.white)
+                        .font(.title.bold())
+                        .background(Color.blue.frame(width: 400, height: 200).ignoresSafeArea())
+                    
+                    List {
+                        ForEach(genres, id: \.self) { genre in
+                            HStack {
+                                Image(systemName: "tag")
+                                    .foregroundColor(.blue)
+                                    .imageScale(.medium)
+                                
+                                Text(genre)
+                                    .foregroundColor(.blue)
+                                    .font(Font.system(size: 20, weight: .bold))
+                            }
                         }
                     }
                 }
