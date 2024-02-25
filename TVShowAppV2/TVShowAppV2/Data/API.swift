d34fd97c230a1d77b01177aa5f5848053603abd9
Modzie <modzielol@gmail.com>
2024-02-25 17:22:00 +0100

    Refactor: put right datas in right files

--- TVShowAppV2/TVShowAppV2/Data/API.swift
+++ TVShowAppV2/TVShowAppV2/Data/API.swift
@@ -7,7 +7,54 @@
 
 import SwiftUI
 
-/* ------- API CLASS FOR SEARCHBAR ------- */
+/* ------- ERRORS HANDLER ------- */
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+}
+
+
+/* ------- STRUCTS FOR JSON ------- */
+struct SearchResult: Codable {
+    let show: Show
+}
+
+struct Show: Codable, Identifiable {
+    let id: Int
+    let name: String
+    let summary: String?
+    let language: String?
+    let status: String?
+    let premiered: String?
+    let ended: String?
+    let genres: [String]?
+    let image: ImageData?
+    let schedule: Schedule?
+    let network: Network?
+}
+
+struct ImageData: Codable {
+    let medium: String?
+    let original: String?
+}
+
+struct Schedule: Codable {
+    let time: String?
+    let days: [String]?
+}
+
+struct Network: Codable {
+    let name: String?
+    let country: Country?
+}
+
+struct Country: Codable {
+    let name: String?
+    let timezone: String?
+}
+
+
+/* ------- CLASS FOR SEARCHBAR ------- */
 class TVMazeAPI {
     static let shared = TVMazeAPI()
     private init() {}
--- TVShowAppV2/TVShowAppV2/Data/Data.swift
+++ TVShowAppV2/TVShowAppV2/Data/Data.swift
@@ -7,53 +7,6 @@
 
 import SwiftUI
 
-/* ------- ERROR FOR API ------- */
-enum NetworkError: Error {
-    case invalidURL
-    case noData
-}
-
-
-/* ------- STRUCTS FOR JSON ------- */
-struct SearchResult: Codable {
-    let show: Show
-}
-
-struct Show: Codable, Identifiable {
-    let id: Int
-    let name: String
-    let summary: String?
-    let language: String?
-    let status: String?
-    let premiered: String?
-    let ended: String?
-    let genres: [String]?
-    let image: ImageData?
-    let schedule: Schedule?
-    let network: Network?
-}
-
-struct ImageData: Codable {
-    let medium: String?
-    let original: String?
-}
-
-struct Schedule: Codable {
-    let time: String?
-    let days: [String]?
-}
-
-struct Network: Codable {
-    let name: String?
-    let country: Country?
-}
-
-struct Country: Codable {
-    let name: String?
-    let timezone: String?
-}
-
-
 /* ------- DISPLAYING SHOWS ------- */
 struct Shows: View {
     @Binding var shows: [Show]
