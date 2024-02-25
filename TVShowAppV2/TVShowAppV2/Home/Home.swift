12cdeca0e621114b5302d138f19cd1abcd4bc08a
Modzie <modzielol@gmail.com>
2024-02-25 13:10:45 +0100

    WIP: Home with just the shows of the current day

--- TVShowAppV2/TVShowAppV2/Home/Home.swift
+++ TVShowAppV2/TVShowAppV2/Home/Home.swift
@@ -8,9 +8,6 @@
 import SwiftUI
 
 struct Home: View {
-    @State private var started = true
-    @State private var isLoading = false
-    @State private var searchTerm = String()
     @State private var shows: [Show] = []
     
     var body: some View {
@@ -20,91 +17,49 @@ struct Home: View {
                     .ignoresSafeArea()
                 
                 ZStack {
-                    VStack(spacing: 0) {
+                    VStack {
                         ZStack {
-                            // Body with TV-series informations
-                            if shows.isEmpty && !started {
-                                VStack {
-                                    Image("Not Found")
-                                        .resizable()
-                                        .frame(width: 250, height: 250)
-                                        .padding()
-                                    Text("No match found...")
-                                        .foregroundColor(.gray)
-                                        .font(Font.system(size: 40))
-                                        .italic()
-                                        .padding()
-                                }
-                            } else {
-                                if self.started {
-                                    VStack {
-                                        Text("Shows of the Day :")
-                                            .foregroundColor(.blue)
-                                            .font(Font.system(size: 30, weight: .bold))
-                                            .offset(y: -50)
-                                        Spacer()
-                                    }
-                                    .padding()
-                                }
-                                ScrollView(.horizontal) {
-                                    HStack(spacing: 10) {
-                                        ForEach(shows) { show in
-                                            NavigationLink(destination: Information(show: show)) {
-                                                if let imageURLString = show.image?.medium {
-                                                    AsyncImage(url: URL(string: imageURLString)) { image in
-                                                        VStack {
-                                                            Text(show.name)
-                                                                .foregroundColor(.blue)
-                                                                .bold()
-                                                                .font(.system(size: 25))
-                                                            image
-                                                                .resizable()
-                                                                .aspectRatio(contentMode: .fit)
-                                                                .frame(maxWidth: .infinity, maxHeight: .infinity)
-                                                                .padding()
-                                                        }
-                                                    } placeholder: {
-                                                        ProgressView()
-                                                    }
+                            VStack {
+                                Text("Shows of the Day :")
+                                    .foregroundColor(.blue)
+                                    .font(Font.system(size: 30, weight: .bold))
+                                    .offset(y: -50)
+                                Spacer()
+                            }
+                            .padding()
+                        }
+                        
+                        ScrollView(.horizontal) {
+                            HStack(spacing: 10) {
+                                ForEach(shows) { show in
+                                    NavigationLink(destination: Information(show: show)) {
+                                        if let imageURLString = show.image?.medium {
+                                            AsyncImage(url: URL(string: imageURLString)) { image in
+                                                VStack {
+                                                    Text(show.name)
+                                                        .foregroundColor(.blue)
+                                                        .bold()
+                                                        .font(.system(size: 25))
+                                                    image
+                                                        .resizable()
+                                                        .aspectRatio(contentMode: .fit)
+                                                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                                        .padding()
                                                 }
+                                            } placeholder: {
+                                                ProgressView()
                                             }
                                         }
                                     }
-                                    .padding()
                                 }
                             }
+                            .padding()
                         }
                     }
-                    
-                    // Loadingscreen for API data
-                    if isLoading {
-                        Color(.blue)
-                            .ignoresSafeArea()
-                        VStack() {
-                            Image("Logo")
-                            
-                            Text("TVShowApp.")
-                                .bold()
-                                .font(Font.system(size: 59, weight: .bold))
-                                .foregroundColor(.white)
-                                .padding()
-                                                        
-                            ProgressView()
-                                .progressViewStyle(CircularProgressViewStyle(tint: .white))
-                                .scaleEffect(3)
-                        }
-                    }
-                    
                 }
-                .onAppear { startLoadingScreen() }
             }
         }
-    }
-    
-    func resetView() {
-        self.searchTerm = String()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            self.started = true
+        .onAppear {
             fetchData()
         }
     }
@@ -125,16 +80,6 @@ struct Home: View {
                 print("Error decoding JSON: \(error.localizedDescription)")
             }
         }.resume()
-    }
-    
-    func startLoadingScreen() {
-        isLoading = true
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
-            isLoading = false
-        }
-        if self.started {
-            fetchData()
-        }
     }
 }
 
