0e33e0b9a43938092e750fc73e6fb3f67add2e13
Modzie <modzielol@gmail.com>
2024-04-25 10:11:15 +0200

    Delete: progression in Better Rest, restart from scratch

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,21 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
-    
     var body: some View {
-        VStack {
-            Stepper("\(self.sleepAmount.formatted()) hours", value: self.$sleepAmount, in: 4...12, step: 0.25)
-            
-            DatePicker("Please enter a date", selection: self.$wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
-                .labelsHidden()
-            
-            Text(Date.now.formatted(date: .long, time: .shortened))
-            
-        }
-        .padding()
     }
 }
 
