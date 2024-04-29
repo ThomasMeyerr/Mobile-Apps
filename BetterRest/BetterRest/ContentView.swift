71ae1bc735fc325c97cce59fa853e968cba4104e
Modzie <modzielol@gmail.com>
2024-04-29 10:25:08 +0200

    Add: ML model to app, and init view for calculate

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,21 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        ZStack {
-            Color.white.ignoresSafeArea()
-            
-            VStack {
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                    .padding()
+        NavigationStack {
+            ZStack {
+                Color.white.ignoresSafeArea()
                 
-                DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-                    .labelsHidden()
+                VStack {
+                    Text("When do you want to wake up ?")
+                        .font(.headline)
+                    
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                    
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                    
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                    
+                    Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                }
+                .navigationTitle("Better Rest")
+                .toolbar {
+                    Button("Calculate", action: calculateBedTime)
+                }
             }
         }
+    }
+    
+    func calculateBedTime() {
+        
     }
 }
 
