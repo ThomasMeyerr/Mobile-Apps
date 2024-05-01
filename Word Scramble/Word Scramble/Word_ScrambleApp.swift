0aaa14c7a12469ff68fc5470c35509e434c8a121
Modzie <modzielol@gmail.com>
2024-05-01 13:06:14 +0200

    Init: Word scramble

---
+++ Word Scramble/Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Thomas Meyer on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
