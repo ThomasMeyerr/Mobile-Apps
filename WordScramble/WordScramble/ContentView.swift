f2f46610cea83714a9c8ba09b86d92e163fb2a8a
Modzie <modzielol@gmail.com>
2024-05-01 13:43:59 +0200

    Testing string

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -9,13 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if let URL = Bundle.main.url(forResource: "some_file", withExtension: "txt") {
+            if let fileContents = try? String (contentsOf: fileURL) {
+                
+            }
         }
-        .padding()
     }
 }
 
