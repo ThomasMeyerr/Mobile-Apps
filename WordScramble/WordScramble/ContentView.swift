c03268a33f47323c175bd0ee2e4620c414edb564
Modzie <modzielol@gmail.com>
2024-05-02 11:14:03 +0200

    Add: base UI for app with textfield

--- WordScramble/WordScramble/ContentView.swift
+++ WordScramble/WordScramble/ContentView.swift
@@ -8,12 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = String()
+    @State private var newWord = String()
+
     var body: some View {
-        if let URL = Bundle.main.url(forResource: "some_file", withExtension: "txt") {
-            if let fileContents = try? String (contentsOf: fileURL) {
+        NavigationStack {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                }
                 
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        Text(word)
+                    }
+                }
             }
         }
+        .navigationTitle(rootWord)
     }
 }
 
