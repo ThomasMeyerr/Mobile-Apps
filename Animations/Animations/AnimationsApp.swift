9b208c33c7137eb1517344843b439f51e2c6f483
Modzie <modzielol@gmail.com>
2024-05-09 12:03:53 +0200

    Init: files for Animations app

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Thomas Meyer on 09/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
