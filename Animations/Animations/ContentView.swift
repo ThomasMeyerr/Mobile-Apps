e230094e7a77466e6b12a3b02f906ea9883e91a5
Modzie <modzielol@gmail.com>
2024-05-09 12:22:45 +0200

    Add: button now increase the size of the same button

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+
     var body: some View {
         Button("Tap Me") {
-            
+            animationAmount += 1
         }
         .padding(50)
         .background(.red)
         .foregroundStyle(.white)
         .clipShape(.circle)
+        .scaleEffect(animationAmount)
     }
 }
 
