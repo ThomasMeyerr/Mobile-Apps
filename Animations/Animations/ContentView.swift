ea6118a1d876279223a7ed47b6b20308c9e308e8
Modzie <modzielol@gmail.com>
2024-05-13 11:04:16 +0200

    Add: drag gesture to move the rect, and back to zero when release

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -8,10 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var dragAmount = CGSize.zero
+
     var body: some View {
         LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .frame(width: 300, height: 200)
+            .frame(width: 100, height: 150)
             .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+                DragGesture()
+                    .onChanged { dragAmount = $0.translation }
+                    .onEnded{ _ in dragAmount = .zero }
+            )
     }
 }
 
