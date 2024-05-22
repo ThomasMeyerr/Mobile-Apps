3a85729c202ee9e87b1bf508d6880d7fc172bddc
Modzie <modzielol@gmail.com>
2024-05-22 10:10:00 +0200

    Refactor: list with a button to add another number to the current list

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -6,44 +6,23 @@
 //
 
 import SwiftUI
-import Observation
-
-@Observable
-class User {
-    var firstName = "Biblo"
-    var lastName = "Baggins"
-}
 
 struct ContentView: View {
-    @State private var user = User()
-    @State private var showingSheet = false
-
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    
     var body: some View {
         VStack {
-            Text("Your name is \(user.firstName) \(user.lastName).")
-            
-            TextField("First name", text: $user.firstName)
-            TextField("Last name", text: $user.lastName)
-            
-            Button("Show Sheet") {
-                showingSheet.toggle()
+            List {
+                ForEach(numbers, id: \.self) {
+                    Text("Row \($0)")
+                }
             }
-        }
-        .padding()
-        .sheet(isPresented: $showingSheet) {
-            SecondView(name: "damn")
-        }
-    }
-}
-
-struct SecondView: View {
-    let name: String
-    
-    @Environment(\.dismiss) var dismiss
-
-    var body: some View {
-        Button("Dismiss") {
-            dismiss()
+            
+            Button("Add Number") {
+                numbers.append(currentNumber)
+                currentNumber += 1
+            }
         }
     }
 }
