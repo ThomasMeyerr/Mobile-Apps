8bfc9680a01d527c6ee321c3275d6efd4ee4db08
Modzie <modzielol@gmail.com>
2024-05-23 09:16:23 +0200

    Add: new file for struct ExpenseItem

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,14 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Thomas Meyer on 23/05/2024.
+//
+
+import SwiftUI
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
