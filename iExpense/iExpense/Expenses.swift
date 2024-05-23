7e1b869caf6d7090b39b71f0b73402f6121174e2
Modzie <modzielol@gmail.com>
2024-05-23 09:18:29 +0200

    Add: class Expenses which has ExpenseItem struct as array

---
+++ iExpense/iExpense/Expenses.swift
@@ -0,0 +1,14 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Thomas Meyer on 23/05/2024.
+//
+
+import SwiftUI
+import Observation
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]()
+}
