c5e6e7d40bd1de90c048166439c2c7c3664a5881
Modzie <modzielol@gmail.com>
2024-05-23 14:37:30 +0200

    Add: Init() func in class Expenses to try to get encoded JSON data from UserDefault, if not : init a void array

--- iExpense/iExpense/Expenses.swift
+++ iExpense/iExpense/Expenses.swift
@@ -10,9 +10,20 @@ import Observation
 
 @Observable
 class Expenses {
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
+    
     var items = [ExpenseItem]() {
         didSet {
-            if let encoded = try? JSONDecoder().encode(items) {
+            if let encoded = try? JSONEncoder().encode(items) {
                 UserDefaults.standard.set(encoded, forKey: "Items")
             }
         }
