5018c056805313b228fd5a52bbd40aa898e04663
Modzie <modzielol@gmail.com>
2024-05-25 23:02:15 +0200

    Add: label to NavigationLink but after the destination to personalize it a bit

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         NavigationStack {
-            NavigationLink("Tap Me") {
+            NavigationLink {
                 Text("Detail View")
+            } label: {
+                VStack {
+                    Text("This is the label")
+                    Text("So is this")
+                    Image(systemName: "face.smiling")
+                }
+                .font(.largeTitle)
             }
-                .navigationTitle("SwiftUI")
         }
     }
 }
