5c0d3f81748b4f7c726ef2794cc76a80778ed4e5
Modzie <modzielol@gmail.com>
2024-05-25 22:33:03 +0200

    Add: ScrollView with VStack to make a view scrollable, and add a frame(maxwidth .infinity) to put the cursor in a better place

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(.example)
+        ScrollView {
+            VStack(spacing: 10) {
+                ForEach(0..<100) {
+                    Text("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
         }
     }
 }
