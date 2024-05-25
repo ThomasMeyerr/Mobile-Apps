4ca83df01c30968e32644ac038a88af25d00efbf
Modzie <modzielol@gmail.com>
2024-05-25 22:56:52 +0200

    Add: add NavigationStack and NavigationTitle to see how it goes

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -9,14 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView {
-            VStack(spacing: 10) {
-                ForEach(0..<100) {
-                    Text("Item \($0)")
-                        .font(.title)
-                }
-            }
-            .frame(maxWidth: .infinity)
+        NavigationStack {
+            Text("Tap Me")
+                .navigationTitle("SwiftUI")
         }
     }
 }
