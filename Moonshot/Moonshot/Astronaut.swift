cb12bf0366bd2e2cda7f0b1e39865ba1d84db9d6
Modzie <modzielol@gmail.com>
2024-05-26 17:38:57 +0200

    Add: extension of Bundle to decode JSON file

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Thomas Meyer on 26/05/2024.
+//
+
+import SwiftUI
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
