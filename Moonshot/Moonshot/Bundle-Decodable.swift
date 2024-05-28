598135b1bdfc0f01cd72f02423160fe0c2f190b1
Modzie <modzielol@gmail.com>
2024-05-28 11:47:02 +0200

    Add: date decoder to parse dates in the exact format we expect

--- Moonshot/Moonshot/Bundle-Decodable.swift
+++ Moonshot/Moonshot/Bundle-Decodable.swift
@@ -17,8 +17,13 @@ extension Bundle {
             fatalError("Failed to load \(file) from bundle.")
         }
         
+        let decoder = JSONDecoder()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "y-MM-dd"
+        decoder.dateDecodingStrategy = .formatted(formatter)
+        
         do {
-            return try JSONDecoder().decode(T.self, from: data)
+            return try decoder.decode(T.self, from: data)
         } catch DecodingError.keyNotFound(let key, let context) {
             fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' – \(context.debugDescription)")
         } catch DecodingError.typeMismatch(_, let context) {
