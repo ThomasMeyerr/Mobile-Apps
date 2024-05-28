aa2de9f70c82e3f477cd2e754383f05c1cc84704
Modzie <modzielol@gmail.com>
2024-05-28 11:43:32 +0200

    Add: advanced layout of main view, using a frame /infinty to make the scroll easier

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -10,21 +10,37 @@ import SwiftUI
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
     
     var body: some View {
         NavigationStack {
-            LazyVStack {
-                ForEach(missions) { mission in
-                    NavigationLink {
-                        Image(mission.image)
-                            .resizable()
-                            .scaledToFit()
-                    } label: {
-                        Text(mission.displayName)
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail View")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.launchDate ?? "N/A")
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
                     }
                 }
             }
-            .navigationTitle("Mission")
+            .navigationTitle("Moonshot")
         }
     }
 }
