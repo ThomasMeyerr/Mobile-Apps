bbaed9875b5be6e91efbdcb9c8d61e64e2390e35
Modzie <modzielol@gmail.com>
2024-05-29 11:43:58 +0200

    Add: init func to see if all astronauts are available in Astronaut data

--- Moonshot/Moonshot/MissionView.swift
+++ Moonshot/Moonshot/MissionView.swift
@@ -16,6 +16,18 @@ struct MissionView: View {
     let mission: Mission
     let crew: [CrewMember]
     
+    init(mission: Mission, astronauts: [String: Astronaut]) {
+        self.mission = mission
+        
+        self.crew = mission.crew.map { member in
+            if let astronaut = astronauts[member.name] {
+                return CrewMember(role: member.role, astronaut: astronaut)
+            } else {
+                fatalError("Missing \(member.name)")
+            }
+        }
+    }
+    
     var body: some View {
         ScrollView {
             VStack {
