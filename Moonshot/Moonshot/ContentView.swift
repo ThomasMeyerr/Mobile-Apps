93b685068443172782c5d03eeee02c625b8cde44
Modzie <modzielol@gmail.com>
2024-05-30 11:05:48 +0200

    Add: button to toolbar to switch between views

--- Moonshot/Moonshot/ContentView.swift
+++ Moonshot/Moonshot/ContentView.swift
@@ -51,10 +51,50 @@ struct GridLayout: View {
     }
 }
 
+struct ListLayout: View {
+    let astronauts: [String: Astronaut]
+    let missions: [Mission]
+    
+    var body: some View {
+        List {
+            ForEach(missions) { mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    VStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .padding()
+                        
+                        VStack {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            Text(mission.formattedLaunchDate)
+                                .font(.caption)
+                                .foregroundStyle(.white.opacity(0.5))
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        .background(.lightBackground)
+                    }
+                    .clipShape(.rect(cornerRadius: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.lightBackground)
+                    )
+                }
+            }
+        }
+        .padding([.horizontal, .bottom])
+    }
+}
+
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
-
     
     @State private var showingGrid = true
     
@@ -64,12 +104,25 @@ struct ContentView: View {
                 Group {
                     if showingGrid {
                         GridLayout(astronauts: astronauts, missions: missions)
+                    } else {
+                        ListLayout(astronauts: astronauts, missions: missions)
                     }
                 }
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                Button {
+                    showingGrid.toggle()
+                } label: {
+                    Image(systemName: "switch.2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 30)
+                        .foregroundStyle(.white)
+                }
+            }
         }
     }
 }
