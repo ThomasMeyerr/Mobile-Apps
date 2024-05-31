f728e011088e9efb277791ab13f99a8959c79601
Modzie <modzielol@gmail.com>
2024-05-31 14:49:10 +0200

    Init: Navigation project

---
+++ Navigation/Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Thomas Meyer on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
