1978087e02de59b5edc583163c81bca2c8325675
Modzie <modzielol@gmail.com>
2024-06-01 14:40:05 +0200

    Add: multiple path in a single button to load multiple views

--- Navigation/Navigation/ContentView.swift
+++ Navigation/Navigation/ContentView.swift
@@ -20,6 +20,10 @@ struct ContentView: View {
                 Button("Show 64") {
                     path = [64]
                 }
+                
+                Button("Show 32 then 64") {
+                    path = [32, 64]
+                }
             }
             .navigationDestination(for: Int.self) { selection in
                 Text("You selected \(selection)")
