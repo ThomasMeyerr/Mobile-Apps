1fbbc4d93df0ae84496f618b134618347175a5bf
Modzie <modzielol@gmail.com>
2024-06-05 10:09:56 +0200

    Init: new project

---
+++ HackingWithSwift/HabitTracking/HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Thomas Meyer on 05/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
