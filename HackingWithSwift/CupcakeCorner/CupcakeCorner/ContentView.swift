1d4458fda16b36aaecba61eaacf868fdd5e920ce
Modzie <modzielol@gmail.com>
2024-06-10 15:06:12 +0200

    Add: import CoreHaptics if we need to dig more into vibrations

--- HackingWithSwift/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ HackingWithSwift/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -6,13 +6,29 @@
 //
 
 import SwiftUI
+import CoreHaptics
 
 struct ContentView: View {
     @State private var counter = Int()
+    @State private var engine: CHHapticEngine?
 
     var body: some View {
         Button("Tap Count: \(counter)") {
             counter += 1
+        }
+        .sensoryFeedback(.impact(weight: .heavy, intensity: 1), trigger: counter)
+    }
+    
+    func prepareHaptics() {
+        guard CHHapticEngine.capabilitiesForHardware().supportsHaptics else {
+            return
+        }
+        
+        do {
+            engine = try CHHapticEngine()
+            try engine?.start()
+        } catch {
+            print("There was an error creating the engine: \(error.localizedDescription)")
         }
     }
 }
