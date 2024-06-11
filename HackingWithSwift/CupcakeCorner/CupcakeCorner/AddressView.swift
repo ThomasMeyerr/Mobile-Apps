ba1fd6fc9a86ec64f97905fbbf31554592879c37
Modzie <modzielol@gmail.com>
2024-06-11 16:09:16 +0200

    Delete: preferredColorScheme in the second view is useless, just in ContentView work

--- HackingWithSwift/CupcakeCorner/CupcakeCorner/AddressView.swift
+++ HackingWithSwift/CupcakeCorner/CupcakeCorner/AddressView.swift
@@ -28,7 +28,6 @@ struct AddressView: View {
         }
         .navigationTitle("Delivery details")
         .navigationBarTitleDisplayMode(.inline)
-        .preferredColorScheme(.dark)
     }
 }
 
