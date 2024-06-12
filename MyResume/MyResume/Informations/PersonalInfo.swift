3ff0cc4e9e3cc1869fc65d518432712b6a452d73
Modzie <modzielol@gmail.com>
2024-06-12 18:10:00 +0200

    Add: Level of languages speaking in PersonalInfo view

--- MyResume/MyResume/Informations/PersonalInfo.swift
+++ MyResume/MyResume/Informations/PersonalInfo.swift
@@ -12,12 +12,19 @@ struct PersonalInfo: View {
 
     var body: some View {
         Form {
-            DisplayTextsInForm(text1: "Nom", text2: data.name)
-            DisplayTextsInForm(text1: "Date de naissance", text2: data.dateOfBirth)
-            DisplayTextsInForm(text1: "Numéro de téléphone", text2: data.phone)
-            DisplayTextsInForm(text1: "Email", text2: data.email)
-            DisplayTextsInForm(text1: "Adresse", text2: data.address)
-            DisplayTextsInForm(text1: "Permis", text2: data.drivingLicense)
+            Section {
+                DisplayTextsInForm(text1: "Nom", text2: data.name)
+                DisplayTextsInForm(text1: "Date de naissance", text2: data.dateOfBirth)
+                DisplayTextsInForm(text1: "Numéro de téléphone", text2: data.phone)
+                DisplayTextsInForm(text1: "Email", text2: data.email)
+                DisplayTextsInForm(text1: "Adresse", text2: data.address)
+                DisplayTextsInForm(text1: "Permis", text2: data.drivingLicense)
+            }
+            
+            Section("Langues") {
+                DisplayTextsInForm(text1: "Français", text2: "C2")
+                DisplayTextsInForm(text1: "Anglais", text2: "B2")
+            }
         }
         .navigationTitle("Informations personnelles")
         .navigationBarTitleDisplayMode(.inline)
