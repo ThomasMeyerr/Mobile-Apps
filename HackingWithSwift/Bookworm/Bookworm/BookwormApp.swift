79fe2c64deb22a44f37cdebfee00d361584664c6
Modzie <modzielol@gmail.com>
2024-06-13 09:58:55 +0200

    Init: New project

---
+++ HackingWithSwift/Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Thomas Meyer on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
