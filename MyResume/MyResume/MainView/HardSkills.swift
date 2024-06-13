989a068267838fbffda8cb73ede69358c180a3ff
Modzie <modzielol@gmail.com>
2024-06-13 14:45:00 +0200

    Create: HardSkills view and complete it

---
+++ MyResume/MyResume/MainView/HardSkills.swift
@@ -0,0 +1,27 @@
+//
+//  HardSkills.swift
+//  MyResume
+//
+//  Created by Thomas Meyer on 13/06/2024.
+//
+
+
+import SwiftUI
+
+struct HardSkills: View {
+    var data: Data
+
+    var body: some View {
+        Form {
+            ForEach(data.hardSkills, id: \.self) {
+                Text($0)
+            }
+        }
+        .navigationTitle("Hard skills")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    HardSkills(data: Data())
+}
