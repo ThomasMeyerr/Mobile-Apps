e3aa95f5f526324c4c6808fb89c1767a632285c4
Modzie <modzielol@gmail.com>
2024-06-14 12:49:02 +0200

    Add: save for day, need some fix

--- HackingWithSwift/Bookworm/Bookworm/ContentView.swift
+++ HackingWithSwift/Bookworm/Bookworm/ContentView.swift
@@ -11,7 +11,6 @@ import SwiftData
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query var books: [Book]
-    
     @State private var showingAddScreen = false
 
     var body: some View {
