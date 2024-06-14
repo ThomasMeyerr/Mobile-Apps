8d88b7b40c9227bc8f0966fbe0992ddb6bd4bf73
Modzie <modzielol@gmail.com>
2024-06-14 12:09:15 +0200

    Add: AddBookView and create a var to handle datas

---
+++ HackingWithSwift/Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,20 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Thomas Meyer on 14/06/2024.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    @Environment(\.modelContext) var modelContext
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
