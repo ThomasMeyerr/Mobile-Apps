d265f0452923bd18476ca79c5803be43e3bd4506
Modzie <modzielol@gmail.com>
2024-06-14 12:25:39 +0200

    Add: base var to handle data

--- HackingWithSwift/Bookworm/Bookworm/ContentView.swift
+++ HackingWithSwift/Bookworm/Bookworm/ContentView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query var books: [Book]
+    
+    @State private var showingAddScreen = false
 
     var body: some View {
         Text("Salut")
