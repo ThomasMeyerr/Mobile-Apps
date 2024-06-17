e82e1f273f51386045d48fb52f51000f7fa4853b
Modzie <modzielol@gmail.com>
2024-06-17 08:54:32 +0200

    Add: RatingView with all their var

---
+++ HackingWithSwift/Bookworm/Bookworm/RatingView.swift
@@ -0,0 +1,30 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Thomas Meyer on 17/06/2024.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    @Binding var rating: Int
+    
+    var label = String()
+    
+    var maximumRating = 5
+    
+    var offImage: Image?
+    var onImage = Image(systemName: "star.fill")
+    
+    var offColor = Color.gray
+    var onColor = Color.yellow
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView(rating: .constant(4))
+}
