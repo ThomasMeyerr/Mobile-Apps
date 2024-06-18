6f125ac9efbe07e7352818f96c01880ed4b7240a
Modzie <modzielol@gmail.com>
2024-06-18 13:46:09 +0200

    Create: DetailView

---
+++ HackingWithSwift/Bookworm/Bookworm/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Thomas Meyer on 18/06/2024.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    
+}
