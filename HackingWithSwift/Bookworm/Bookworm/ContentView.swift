494695faa2e33db55a9523027833c8366aa881bb
Modzie <modzielol@gmail.com>
2024-06-18 15:44:28 +0200

    Add: possibility to sort our data came from SwiftData using Query

--- HackingWithSwift/Bookworm/Bookworm/ContentView.swift
+++ HackingWithSwift/Bookworm/Bookworm/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) var modelContext
-    @Query var books: [Book]
+    @Query(sort: [SortDescriptor(\Book.title)]) var books: [Book]
     @State private var showingAddScreen = false
 
     var body: some View {
