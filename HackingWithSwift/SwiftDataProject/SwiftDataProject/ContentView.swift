e97c97f9747c8f046fc78c33f13ce596693bdf8d
Modzie <modzielol@gmail.com>
2024-06-20 12:28:55 +0200

    Add: properties to get access to SwiftData and a private var path to take data before sending it to model

--- HackingWithSwift/SwiftDataProject/SwiftDataProject/ContentView.swift
+++ HackingWithSwift/SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -5,9 +5,14 @@
 //  Created by Thomas Meyer on 20/06/2024.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query(sort: \User.name) var users: [User]
+    @State private var path = [User]()
+
     var body: some View {
         VStack {
             Image(systemName: "globe")
