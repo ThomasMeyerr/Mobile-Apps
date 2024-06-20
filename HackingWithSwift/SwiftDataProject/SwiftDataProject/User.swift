b83c272c494d3625521661f882404b5a2d7c1b95
Modzie <modzielol@gmail.com>
2024-06-20 12:17:15 +0200

    Add: User file with a Model class

---
+++ HackingWithSwift/SwiftDataProject/SwiftDataProject/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  SwiftDataProject
+//
+//  Created by Thomas Meyer on 20/06/2024.
+//
+
+import SwiftData
+import SwiftUI
+
+@Model
+class User {
+    var name: String
+    var city: String
+    var joinDate: Date
+    
+    init(name: String, city: String, joinDate: Date) {
+        self.name = name
+        self.city = city
+        self.joinDate = joinDate
+    }
+}
