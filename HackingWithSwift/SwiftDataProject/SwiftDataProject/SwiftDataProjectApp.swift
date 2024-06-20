00a1b4093a1cf2b66748e9cc2e1ac966d178676d
Modzie <modzielol@gmail.com>
2024-06-20 10:39:17 +0200

    Init: SwiftDataProject files

---
+++ HackingWithSwift/SwiftDataProject/SwiftDataProject/SwiftDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataProjectApp.swift
+//  SwiftDataProject
+//
+//  Created by Thomas Meyer on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
