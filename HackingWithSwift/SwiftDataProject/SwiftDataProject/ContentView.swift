d941e7efb7ad3d49508c4f9ba801e7b6944e716d
Modzie <modzielol@gmail.com>
2024-06-23 14:22:02 +0200

    Add: func to sample job and User to test it

--- HackingWithSwift/SwiftDataProject/SwiftDataProject/ContentView.swift
+++ HackingWithSwift/SwiftDataProject/SwiftDataProject/ContentView.swift
@@ -55,9 +55,22 @@ struct ContentView: View {
                     modelContext.insert(second)
                     modelContext.insert(third)
                     modelContext.insert(fourth)
+                    
+                    addSample()
                 }
             }
         }
+    }
+    
+    func addSample() {
+        let user1 = User(name: "Piper Chapman", city: "New York", joinDate: .now)
+        let job1 = Job(name: "Organize sock drawer", priority: 3)
+        let job2 = Job(name: "Make plans with Alex", priority: 4)
+        
+        modelContext.insert(user1)
+        
+        user1.jobs.append(job1)
+        user1.jobs.append(job2)
     }
 }
 
