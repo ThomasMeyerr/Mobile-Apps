283cda038fbf4a5c7d8775010335da8a2db9bc5f
Modzie <modzielol@gmail.com>
2024-06-26 12:21:59 +0200

    Add: User and Friend class to store JSON data

---
+++ HackingWithSwift/ChallengeDay60/ChallengeDay60/Friend.swift
@@ -0,0 +1,8 @@
+//
+//  Friend.swift
+//  ChallengeDay60
+//
+//  Created by Thomas Meyer on 26/06/2024.
+//
+
+import Foundation
---
+++ HackingWithSwift/ChallengeDay60/ChallengeDay60/User.swift
@@ -0,0 +1,8 @@
+//
+//  User.swift
+//  ChallengeDay60
+//
+//  Created by Thomas Meyer on 26/06/2024.
+//
+
+import Foundation
