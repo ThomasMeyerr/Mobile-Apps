6a2eddbe2a693b327998e963f915b2d80a0ae5af
Modzie <modzielol@gmail.com>
2024-06-26 12:02:42 +0200

    Init: project for today challenge

---
+++ HackingWithSwift/ChallengeDay60/ChallengeDay60/ChallengeDay60App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay60App.swift
+//  ChallengeDay60
+//
+//  Created by Thomas Meyer on 26/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay60App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
