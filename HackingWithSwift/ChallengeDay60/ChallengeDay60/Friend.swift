6db7e1782afd7bda5a710ea96b81f26de037b0c0
Modzie <modzielol@gmail.com>
2024-06-26 12:25:59 +0200

    Add: variable for classes with init func to correspond to Model for SwiftData

--- HackingWithSwift/ChallengeDay60/ChallengeDay60/Friend.swift
+++ HackingWithSwift/ChallengeDay60/ChallengeDay60/Friend.swift
@@ -6,3 +6,15 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Friend {
+    var id: UUID
+    var name: String
+    
+    init(id: UUID, name: String) {
+        self.id = id
+        self.name = name
+    }
+}
--- HackingWithSwift/ChallengeDay60/ChallengeDay60/User.swift
+++ HackingWithSwift/ChallengeDay60/ChallengeDay60/User.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class User {
+    var id: UUID
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: String
+    var tags: [String]
+    var friends: [Friend]
+    
+    init(id: UUID, isActive: Bool, name: String, age: Int, company: String, email: String, address: String, about: String, registered: String, tags: [String], friends: [Friend]) {
+        self.id = id
+        self.isActive = isActive
+        self.name = name
+        self.age = age
+        self.company = company
+        self.email = email
+        self.address = address
+        self.about = about
+        self.registered = registered
+        self.tags = tags
+        self.friends = friends
+    }
+}
