b8cd207f83ceca541550def3698591cdc503cb0d
Modzie <modzielol@gmail.com>
2024-06-27 10:21:54 +0200

    Add: struct to class to change the app with SwiftData

--- HackingWithSwift/ChallengeDay60/ChallengeDay60/User.swift
+++ HackingWithSwift/ChallengeDay60/ChallengeDay60/User.swift
@@ -6,8 +6,24 @@
 //
 
 import Foundation
+import SwiftData
 
-struct User: Codable, Identifiable {
+@Model
+class User: Codable {
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case isActive = "isActive"
+        case name = "name"
+        case age = "age"
+        case company = "company"
+        case email = "email"
+        case address = "address"
+        case about = "about"
+        case registered = "registered"
+        case tags = "tags"
+        case friends = "friends"
+    }
+    
     var id: UUID
     var isActive: Bool
     var name: String
@@ -19,4 +35,16 @@ struct User: Codable, Identifiable {
     var registered: Date
     var tags: [String]
     var friends: [Friend]
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.isActive = try container.decode(Bool.self, forKey: .isActive)
+        
+        
+    }
+    
+    func encode(to encoder: Encoder) throws {
+        <#code#>
+    }
 }
