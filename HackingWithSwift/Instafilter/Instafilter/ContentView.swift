e61c4d56fafd5613dcc19abdf90bc8ba48744e87
Modzie <modzielol@gmail.com>
2024-06-28 10:40:32 +0200

    Add: basic view to check slider view to choose our blur

--- HackingWithSwift/Instafilter/Instafilter/ContentView.swift
+++ HackingWithSwift/Instafilter/Instafilter/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var blurAmount = Double()
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Wolrd!")
+                .blur(radius: blurAmount)
+            
+            Slider(value: $blurAmount, in: 0...20)
+                .padding()
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
         }
-        .padding()
     }
 }
 
