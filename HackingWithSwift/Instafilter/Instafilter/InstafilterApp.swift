d15cf7e329e9455139130ada49fcee866ed0fd1c
Modzie <modzielol@gmail.com>
2024-06-28 09:57:17 +0200

    Init: new project

---
+++ HackingWithSwift/Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Thomas Meyer on 28/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
