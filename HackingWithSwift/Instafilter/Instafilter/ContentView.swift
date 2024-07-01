12ca40042ea6e00075c8887cdf95cb7eb91af6f6
Modzie <modzielol@gmail.com>
2024-07-01 11:30:50 +0200

    Add: all conversions to get a swiftui image with a sepiatone applied

--- HackingWithSwift/Instafilter/Instafilter/ContentView.swift
+++ HackingWithSwift/Instafilter/Instafilter/ContentView.swift
@@ -29,6 +29,18 @@ struct ContentView: View {
         let currentFilter = CIFilter.sepiaTone()
         currentFilter.inputImage = beginImage
         currentFilter.intensity = 1
+        
+        // Get a CIImage from our filter or exit if that fails
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        // Attempt to get a CGImage from our CIImage
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        // Convert that to a UIImage
+        let uiImage = UIImage(cgImage: cgImage)
+        
+        // And convert that to a SwiftUI image
+        image = Image(uiImage: uiImage)
     }
 }
 
