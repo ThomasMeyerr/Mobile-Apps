92fc4f3c61663657bfb9b2fea0f635975e885e41
Modzie <modzielol@gmail.com>
2024-07-02 15:35:01 +0200

    ⏪️ Update:  Sharelink view to share any data we want an ios provide each app which could handle the type of data we want to share

--- HackingWithSwift/Instafilter/Instafilter/ContentView.swift
+++ HackingWithSwift/Instafilter/Instafilter/ContentView.swift
@@ -9,34 +9,9 @@ import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var pickerItem = [PhotosPickerItem]()
-    @State private var selectedImage = [Image]()
 
     var body: some View {
-        VStack {
-            ScrollView {
-                ForEach(0..<selectedImage.count, id: \.self) { index in
-                    selectedImage[index]
-                        .resizable()
-                        .scaledToFit()
-                }
-            }
-        
-            PhotosPicker(selection: $pickerItem, maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)])) {
-                Label("Select images", systemImage: "photo")
-            }
-        }
-        .onChange(of: pickerItem) {
-            Task {
-                selectedImage.removeAll()
-                
-                for item in pickerItem {
-                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
-                        selectedImage.append(loadedImage)
-                    }
-                }
-            }
-        }
+        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!)
     }
 }
 
