6dce40ac0287c3391bf87300ae33594eef74d2fb
Modzie <modzielol@gmail.com>
2024-07-02 11:10:25 +0200

    ⏪️ Update: PhotosUI to learn it

--- HackingWithSwift/Instafilter/Instafilter/ContentView.swift
+++ HackingWithSwift/Instafilter/Instafilter/ContentView.swift
@@ -5,60 +5,15 @@
 //  Created by Thomas Meyer on 28/06/2024.
 //
 
-import CoreImage
-import CoreImage.CIFilterBuiltins
+import PhotosUI
 import SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
+    @State private var pickerItem: PhotosPickerItem?
+    @State private var selectedImage: Image?
 
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFit()
-            
-            ContentUnavailableView {
-                Label("No Snippets", systemImage: "swift")
-            } description: {
-                Text("You don't have any snippets yet.")
-            } actions: {
-                Button("Create snippet") {
-                    
-                }
-                .buttonStyle(.borderedProminent)
-            }
-        }
-        .onAppear(perform: loadImage)
-    }
-    
-    func loadImage() {
-        let inputImage = UIImage(resource: .example)
-        let beginImage = CIImage(image: inputImage)
         
-        let context = CIContext()
-        let currentFilter = CIFilter.sepiaTone()
-        currentFilter.inputImage = beginImage
-        
-        let amount = 1.0
-        
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)}
-        
-        // Get a CIImage from our filter or exit if that fails
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        // Attempt to get a CGImage from our CIImage
-        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-        
-        // Convert that to a UIImage
-        let uiImage = UIImage(cgImage: cgImage)
-        
-        // And convert that to a SwiftUI image
-        image = Image(uiImage: uiImage)
     }
 }
 
