2b6df1e4887e4efa47ba1282e3ac36f3c6d4443a
Modzie <modzielol@gmail.com>
2024-07-09 15:25:06 +0200

    Add: basic array with sorted() modifier to verify it actually works

--- HackingWithSwift/BucketList/BucketList/ContentView.swift
+++ HackingWithSwift/BucketList/BucketList/ContentView.swift
@@ -8,14 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    let values = [1, 5, 3, 6, 2, 9].sorted()
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List(values, id: \.self) {
+            Text(String($0))
         }
-        .padding()
     }
 }
 
