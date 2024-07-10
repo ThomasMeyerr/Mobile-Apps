345d9e0fbe074d75f44174c8e9bf1e6b063c69e5
Modzie <modzielol@gmail.com>
2024-07-10 16:47:43 +0200

    Add: realistic and hybrid map with a start position in Paris(France)

--- HackingWithSwift/BucketList/BucketList/ContentView.swift
+++ HackingWithSwift/BucketList/BucketList/ContentView.swift
@@ -9,9 +9,16 @@ import MapKit
 import SwiftUI
 
 struct ContentView: View {
+    let startPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 48.50, longitude: 2.20),
+            span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+        )
+    )
     
     var body: some View {
-        
+        Map(initialPosition: startPosition)
+            .mapStyle(.hybrid(elevation: .realistic))
     }
 }
 
