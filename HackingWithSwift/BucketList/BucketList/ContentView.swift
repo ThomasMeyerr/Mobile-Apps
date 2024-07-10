a6ac55c877e8618ae831fff8d1505e6c18796aab
Modzie <modzielol@gmail.com>
2024-07-10 11:44:32 +0200

    Add: method to ContentView to check whether a biometric authentication is available

--- HackingWithSwift/BucketList/BucketList/ContentView.swift
+++ HackingWithSwift/BucketList/BucketList/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by Thomas Meyer on 09/07/2024.
 //
 
+import LocalAuthentication
 import SwiftUI
 
 struct ContentView: View {
@@ -12,6 +13,27 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
+        }
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    // authenticated successfully
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
         }
     }
 }
