20a8863ada2e203a3717e48176cd9c30ebae0374
Modzie <modzielol@gmail.com>
2024-07-10 10:51:25 +0200

    Add: a key to info tab of our app settings to add Privac - Face ID and a value which is 'we need to unlock your data', swift need to know how we need face id

--- HackingWithSwift/BucketList/BucketList/ContentView.swift
+++ HackingWithSwift/BucketList/BucketList/ContentView.swift
@@ -5,43 +5,13 @@
 //  Created by Thomas Meyer on 09/07/2024.
 //
 
-import MapKit
 import SwiftUI
 
-struct Location: Identifiable {
-    let id = UUID()
-    var name: String
-    var coordinate: CLLocationCoordinate2D
-}
-
 struct ContentView: View {
-    let locations = [
-        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
-        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
-    ]
     
     var body: some View {
-//        Map {
-//            ForEach(locations) { location in
-//                Annotation(location.name, coordinate: location.coordinate) {
-//                    Text(location.name)
-//                        .font(.headline)
-//                        .padding()
-//                        .background(.blue)
-//                        .foregroundStyle(.white)
-//                        .clipShape(.capsule)
-//                }
-//                .annotationTitles(.hidden)
-//            }
-//        }
-        
-        MapReader { proxy in
-            Map()
-                .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local) {
-                        print(coordinate)
-                    }
-                }
+        VStack {
+            
         }
     }
 }
