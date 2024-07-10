58f25f115200b9ae9e3abbefd98eb9624c41b7a4
Modzie <modzielol@gmail.com>
2024-07-10 09:55:41 +0200

    Add: Map view with MapKit to just display iOS map

--- HackingWithSwift/BucketList/BucketList/ContentView.swift
+++ HackingWithSwift/BucketList/BucketList/ContentView.swift
@@ -5,42 +5,13 @@
 //  Created by Thomas Meyer on 09/07/2024.
 //
 
+import MapKit
 import SwiftUI
 
-enum LoadingState {
-    case loading, success, failed
-}
-
-struct LoadingView: View {
-    var body: some View {
-        Text("Loading...")
-    }
-}
-
-struct SuccessView: View {
-    var body: some View {
-        Text("Success !")
-    }
-}
-
-struct FailedView: View {
-    var body: some View {
-        Text("Failed.")
-    }
-}
-
 struct ContentView: View {
-    @State private var loadingState = LoadingState.loading
     
     var body: some View {
-        switch loadingState {
-        case .loading:
-            LoadingView()
-        case .success:
-            SuccessView()
-        case .failed:
-            FailedView()
-        }
+        Map()
     }
 }
 
