6b32302a995cd5fab1f2709fe4cbffed066a87ac
Modzie <modzielol@gmail.com>
2024-07-11 09:10:30 +0200

    Add: fetchNearbyPlaces func to fetch wikipedia api with our latitude and longitude to find nearby's places, and switch case for loadingState depending on results

--- HackingWithSwift/BucketList/BucketList/EditView.swift
+++ HackingWithSwift/BucketList/BucketList/EditView.swift
@@ -58,6 +58,32 @@ struct EditView: View {
                     dismiss()
                 }
             }
+            .task {
+                await fetchNearbyPlaces()
+            }
+        }
+    }
+    
+    func fetchNearbyPlaces() async {
+        let urlString = "https://en.wikipedia.org/w/api.php?ggscoord=\(location.latitude)%7C\(location.longitude)&action=query&prop=coordinates%7Cpageimages%7Cpageterms&colimit=50&piprop=thumbnail&pithumbsize=500&pilimit=50&wbptterms=description&generator=geosearch&ggsradius=10000&ggslimit=50&format=json"
+        
+        guard let url = URL(string: urlString) else {
+            print("Bad URL: \(urlString)")
+            return
+        }
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // we get some data back !
+            let items = try JSONDecoder().decode(Result.self, from: data)
+            
+            // success - convert the array values to our pages array
+            pages = items.query.pages.values.sorted { $0.title < $1.title }
+            loadingState = .loaded
+        } catch {
+            // if we're still here it means the request failed somehow
+            loadingState = .failed
         }
     }
     
