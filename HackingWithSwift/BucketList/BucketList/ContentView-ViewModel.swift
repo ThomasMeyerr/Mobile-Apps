1a10d2fb2d9aa254318c48e9d4584111fcc9e8e2
Modzie <modzielol@gmail.com>
2024-07-12 10:34:40 +0200

    Init: file to have a better structure for our app following MVVM model

---
+++ HackingWithSwift/BucketList/BucketList/ContentView-ViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView-ViewModel.swift
+//  BucketList
+//
+//  Created by Thomas Meyer on 12/07/2024.
+//
+
+import Foundation
+import MapKit
+
+extension ContentView {
+    @Observable
+    class ViewModel {
+    }
+}
