8182cd133f2bed415eb5861d8e2a7d5ba88e8469
Modzie <modzielol@gmail.com>
2024-07-15 17:06:22 +0200

    Add: these button inside view will work perfectly for regular users but for VoiceOver it will just said increment and decrement

--- HackingWithSwift/AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
+++ HackingWithSwift/AccessibilitySandbox/AccessibilitySandbox/ContentView.swift
@@ -8,29 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    let pictures = [
-        "ales-krivec-15949",
-        "galina-n-189483",
-        "kevin-horstmann-141705",
-        "nicolas-tissot-335096"
-    ]
-    let labels = [
-        "Tulips",
-        "Frozen tree buds",
-        "Sunflowers",
-        "Fireworks"
-    ]
-    
-    @State private var selectedPicture = Int.random(in: 0...3)
+    @State private var value = 10
     
     var body: some View {
         VStack {
-            Text("Your score is")
-            Text("1000")
-                .font(.title)
+            Text("Value: \(value)")
+            
+            Button("Increment") {
+                value += 1
+            }
+            
+            Button("Decrement") {
+                value -= 1
+            }
         }
-        .accessibilityElement(children: .ignore)
-        .accessibilityLabel("Your score is 1000")
     }
 }
 
