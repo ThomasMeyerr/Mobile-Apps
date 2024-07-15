a0d9d39174b0d8d3cf9d67c546618e546583b0fe
Modzie <modzielol@gmail.com>
2024-07-15 15:24:15 +0200

    Init: new project

---
+++ HackingWithSwift/AccessibilitySandbox/AccessibilitySandbox/AccessibilitySandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilitySandboxApp.swift
+//  AccessibilitySandbox
+//
+//  Created by Thomas Meyer on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilitySandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
