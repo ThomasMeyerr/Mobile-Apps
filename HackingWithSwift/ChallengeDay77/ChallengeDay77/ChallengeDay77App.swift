53e56bc0e62cb9d11a8ed2ab8ff89ba447d5bf9a
Modzie <modzielol@gmail.com>
2024-07-18 11:26:38 +0200

    Init: new project

---
+++ HackingWithSwift/ChallengeDay77/ChallengeDay77/ChallengeDay77App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay77App.swift
+//  ChallengeDay77
+//
+//  Created by Thomas Meyer on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay77App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
