d16bdd7f87f8cd730c97d5c1d5ae0a8ce005379f
Modzie <modzielol@gmail.com>
2024-07-18 11:49:37 +0200

    Add: operator overload for sorted func, try to init saveData

--- HackingWithSwift/ChallengeDay77/ChallengeDay77/ContentView.swift
+++ HackingWithSwift/ChallengeDay77/ChallengeDay77/ContentView.swift
@@ -5,17 +5,38 @@
 //  Created by Thomas Meyer on 18/07/2024.
 //
 
+import PhotosUI
 import SwiftUI
 
-struct Photo: Identifiable {
+struct Photo: Identifiable, Codable, Comparable {
     let id = UUID()
     var name: String
     var description: String
+    var image: Image?
+    
+    init() {
+        do {
+            data =
+            decoded = try JSONDecoder().decode(Photo.self, from: data)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    static func <(lhs: Photo, rhs: Photo) -> Bool {
+        lhs.name < rhs.name
+    }
 }
 
+struct AddPhoto: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
 
 struct ContentView: View {
     @State private var photos = [Photo]()
+    @State private var isSelected = false
 
     var body: some View {
         NavigationStack {
@@ -30,15 +51,16 @@ struct ContentView: View {
             }
             .navigationTitle("Photo saver")
             .toolbar {
-                Button(action: addPhoto, label: {
+                Button {
+                    isSelected = true
+                } label: {
                     Image(systemName: "plus")
-                })
+                }
+            }
+            .sheet(isPresented: $isSelected) {
+                AddPhoto()
             }
         }
-    }
-                       
-    func addPhoto() {
-                
     }
 }
 
