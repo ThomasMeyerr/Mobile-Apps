dcfdd943ec28b7133c701d95c4b4a270f6033617
Modzie <modzielol@gmail.com>
2024-07-19 09:26:32 +0200

    init: file to get location from imported image

---
+++ HackingWithSwift/ChallengeDay77/ChallengeDay77/LocationFetcher.swift
@@ -0,0 +1,13 @@
+//
+//  LocationFetcher.swift
+//  ChallengeDay77
+//
+//  Created by Thomas Meyer on 19/07/2024.
+//
+
+import CoreLocation
+import Foundation
+
+class LocationFetcher: NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+}
