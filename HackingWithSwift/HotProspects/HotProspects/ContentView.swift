b78d1ac18ee3c56267653676201c7bb5362d6ad1
Modzie <modzielol@gmail.com>
2024-07-30 16:08:35 +0200

    Change: optional to a set to store multiple string, set to avoid doublon

--- HackingWithSwift/HotProspects/HotProspects/ContentView.swift
+++ HackingWithSwift/HotProspects/HotProspects/ContentView.swift
@@ -9,15 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     let users = ["Tohru", "Yuki", "Kyo", "Momiji"]
-    @State private var selection: String?
+    @State private var selection = Set<String>()
     
     var body: some View {
         List(users, id: \.self, selection: $selection) { user in
             Text(user)
         }
         
-        if let selection {
-            Text("You selected \(selection)")
+        if !selection.isEmpty {
+            Text("You selected \(selection.formatted())")
         }
     }
 }
