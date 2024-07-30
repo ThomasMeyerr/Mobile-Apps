9ddfa85080ab0ecc31954fefe9e0b373f0f6b0c8
Modzie <modzielol@gmail.com>
2024-07-30 15:57:15 +0200

    Init: new project

---
+++ HackingWithSwift/HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Thomas Meyer on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
