974155370f6ff197d4495ebcb3430d4e456d38eb
Modzie <modzielol@gmail.com>
2024-07-31 09:09:11 +0200

    Add: tag modifier with a selection paramater to our tabView to know on which tab we are and switch tab by clicking a button in a view

--- HackingWithSwift/HotProspects/HotProspects/ContentView.swift
+++ HackingWithSwift/HotProspects/HotProspects/ContentView.swift
@@ -9,17 +9,23 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var selectedTab = "One"
+    
     var body: some View {
-        TabView {
-            Text("1")
-                .tabItem {
-                    Label("One", systemImage: "star")
-                }
-            
-            Text("2")
+        TabView(selection: $selectedTab) {
+            Button("Show Tab 2") {
+                selectedTab = "Two"
+            }
+            .tabItem {
+                Label("One", systemImage: "star")
+            }
+            .tag("One")
+
+            Text("Tab 2")
                 .tabItem {
                     Label("Two", systemImage: "circle")
                 }
+                .tag("Two")
         }
     }
 }
