72c4e7768c03536f3446205c60bf8536179b93ca
Modzie <modzielol@gmail.com>
2024-08-02 10:25:05 +0200

    Add: swipeActions modifier to a List to trigger a swip, with a systemImage, in gray by default, when clicking on it, an action is triggered

--- HackingWithSwift/HotProspects/HotProspects/ContentView.swift
+++ HackingWithSwift/HotProspects/HotProspects/ContentView.swift
@@ -8,25 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var backgroundColor = Color.red
 
     var body: some View {
-        VStack {
-            Text("Hello, World!")
-                .padding()
-                .background(backgroundColor)
-            
-            Text("Change color")
-                .padding()
-                .contextMenu {
-                    Button("Red", systemImage: "checkmark.circle.fill", role: .destructive) {
-                        backgroundColor = .red
-                    }
-                    Button("Green") {
-                        backgroundColor = .green
-                    }
-                    Button("Blue") {
-                        backgroundColor = .blue
+        List {
+            Text("Taylor Swift")
+                .swipeActions {
+                    Button("Send message", systemImage: "message") {
+                        print("Hi")
                     }
                 }
         }
