1ed954e84d1e6d524e340cddb0419157bb09d95d
Modzie <modzielol@gmail.com>
2024-08-02 12:49:01 +0200

    Refactor: back to original state

--- HackingWithSwift/HotProspects/HotProspects/ContentView.swift
+++ HackingWithSwift/HotProspects/HotProspects/ContentView.swift
@@ -5,19 +5,12 @@
 //  Created by Thomas Meyer on 30/07/2024.
 //
 
-import SamplePackage
 import SwiftUI
 
 struct ContentView: View {
-    let possibleNumbers = 1...60
-    var results: String {
-        let selected = possibleNumbers.random(7).sorted()
-        let strings = selected.map(String.init)
-        return strings.formatted()
-    }
 
     var body: some View {
-        Text(results)
+        Text("Hello, world!")
     }
 }
 
