12da52a651b59056ddc702d56d1a0d527a61d204
Modzie <modzielol@gmail.com>
2024-08-04 18:16:59 +0200

    Add: placeholders to ContentView in a tabBar

--- HackingWithSwift/HotProspects/HotProspects/ContentView.swift
+++ HackingWithSwift/HotProspects/HotProspects/ContentView.swift
@@ -10,7 +10,24 @@ import SwiftUI
 struct ContentView: View {
 
     var body: some View {
-        Text("Hello, world!")
+        TabView {
+            ProspectsView()
+                .tabItem {
+                    Label("Everyone", systemImage: "person.3")
+                }
+            ProspectsView()
+                .tabItem {
+                    Label("Contacted", systemImage: "checkmark.circle")
+                }
+            ProspectsView()
+                .tabItem {
+                    Label("Uncontacted", systemImage: "questionmark.diamond")
+                }
+            MeView()
+                .tabItem {
+                    Label("Me", systemImage: "person.crop.square")
+                }
+        }
     }
 }
 
