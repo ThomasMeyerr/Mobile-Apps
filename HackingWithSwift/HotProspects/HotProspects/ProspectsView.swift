abc2c23f32117f353faa4ca8f6ca4de6384dbcb4
Modzie <modzielol@gmail.com>
2024-08-04 18:31:21 +0200

    Add: query to sort and use an array of prospect, and have an environnement var to store value. Add modelContainer to previews to use it in developpement

--- HackingWithSwift/HotProspects/HotProspects/ProspectsView.swift
+++ HackingWithSwift/HotProspects/HotProspects/ProspectsView.swift
@@ -5,12 +5,16 @@
 //  Created by Thomas Meyer on 04/08/2024.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ProspectsView: View {
     enum FilterType {
         case none, contacted, uncontacted
     }
+    
+    @Query(sort: \Prospect.name) var prospects: [Prospect]
+    @Environment(\.modelContext) var modelContext
     
     let filter: FilterType
     var title: String {
@@ -34,4 +38,5 @@ struct ProspectsView: View {
 
 #Preview {
     ProspectsView(filter: .none)
+        .modelContainer(for: Prospect.self)
 }
