573579932313425a109826117f6f8929db3c285d
Modzie <modzielol@gmail.com>
2024-08-04 18:27:51 +0200

    Update: app file to declare a model container for the class Prospect

--- HackingWithSwift/HotProspects/HotProspects/HotProspectsApp.swift
+++ HackingWithSwift/HotProspects/HotProspects/HotProspectsApp.swift
@@ -5,6 +5,7 @@
 //  Created by Thomas Meyer on 30/07/2024.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct HotProspectsApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: Prospect.self)
     }
 }
