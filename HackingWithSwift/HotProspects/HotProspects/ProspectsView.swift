de75b1bdabbf6a149411eff213e9d3268b5b74f4
Modzie <modzielol@gmail.com>
2024-08-04 18:11:47 +0200

    Init: 2 placeholders views

---
+++ HackingWithSwift/HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Thomas Meyer on 04/08/2024.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProspectsView()
+}
