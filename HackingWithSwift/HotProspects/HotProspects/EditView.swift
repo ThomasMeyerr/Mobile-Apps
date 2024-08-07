6727e04df8dd33d0323d7ee902d9f33d1a6e02c5
Modzie <modzielol@gmail.com>
2024-08-07 10:39:52 +0200

    Init: EditView

---
+++ HackingWithSwift/HotProspects/HotProspects/EditView.swift
@@ -0,0 +1,18 @@
+//
+//  EditView.swift
+//  HotProspects
+//
+//  Created by Thomas Meyer on 07/08/2024.
+//
+
+import SwiftUI
+
+struct EditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditView()
+}
