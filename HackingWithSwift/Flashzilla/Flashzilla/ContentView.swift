194c0e76c828f75e877bcbc59198596731ce462b
Modzie <modzielol@gmail.com>
2024-08-08 14:38:04 +0200

    Add: Spacer in a VStack is not considered like a tappable space

--- HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
+++ HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
@@ -10,21 +10,13 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(.blue)
-                .frame(width: 300, height: 300)
-                .onTapGesture {
-                    print("Rectangle tapped")
-                }
-            
-            Circle()
-                .fill(.red)
-                .frame(width: 300, height: 300)
-                .contentShape(.rect)
-                .onTapGesture {
-                    print("Circle tapped")
-                }
+        VStack {
+            Text("Hello")
+            Spacer().frame(height: 100)
+            Text("World")
+        }
+        .onTapGesture {
+            print("VStack tapped!")
         }
     }
 }
