033ac017f454d2e23b03a3246ce96fb2df28b13c
Modzie <modzielol@gmail.com>
2024-08-08 11:09:11 +0200

    Init: new project

---
+++ HackingWithSwift/Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Thomas Meyer on 08/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
