89bc9dbcb177ab3113636796f2a307b500b39edc
Modzie <modzielol@gmail.com>
2024-08-08 11:49:23 +0200

    Add: gesture modifier to handle precise gesture in a wrap, every gesture have a onChange and onEnded modifier

--- HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
+++ HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var currentAmount = 0.0
+    @State private var finalAmount = 1.0
+    
     var body: some View {
         Text("Hello, world!")
-            .onLongPressGesture(minimumDuration: 1) {
-                print("Long pressed !")
-            } onPressingChanged: { inProgress in
-                print("In progress: \(inProgress)!")
-            }
+            .scaleEffect(finalAmount + currentAmount)
+            .gesture(
+                MagnifyGesture()
+                    .onChanged { value in
+                        currentAmount = value.magnification - 1
+                    }
+                    .onEnded { value in
+                        finalAmount += currentAmount
+                        currentAmount = 0
+                    }
+            )
     }
 }
 
