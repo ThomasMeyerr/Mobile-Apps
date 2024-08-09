a7b7347a0ca1fd2ceed5d6d5f197c36b195a48bf
Modzie <modzielol@gmail.com>
2024-08-09 09:38:16 +0200

    Add: onReceive modifier to have a publisher as first parameter to show everytime timer var throw sthg

--- HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
+++ HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
@@ -11,7 +11,10 @@ struct ContentView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        
+        Text("Hello, world!")
+            .onReceive(timer) { time in
+                print("The time is now \(time)")
+            }
     }
 }
 
