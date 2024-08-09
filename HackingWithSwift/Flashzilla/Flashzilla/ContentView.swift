2a2f99a219cb11d2ef228dfbc968275c161bdb25
Modzie <modzielol@gmail.com>
2024-08-09 10:37:44 +0200

    Update: same with reduceMotion accessibility, with this true app should limit the amount of animation causes movement on screen

--- HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
+++ HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
@@ -8,20 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityReduceMotion) var reduceMotion
+    @State private var scale = 1.0
     
     var body: some View {
-        HStack {
-            if differentiateWithoutColor {
-                Image(systemName: "checkmark.circle")
+        Button("Hello, world!") {
+            if reduceMotion {
+                scale *= 1.5
+            } else {
+                withAnimation {
+                    scale *= 1.5
+                }
             }
-            
-            Text("Success")
         }
-        .padding()
-        .background(differentiateWithoutColor ? .black : .green)
-        .foregroundStyle(.white)
-        .clipShape(.capsule)
+        .scaleEffect(scale)
     }
 }
 
