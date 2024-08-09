23f4c4b3d9306fcc5207f4deae8c3a3503af0d96
Modzie <modzielol@gmail.com>
2024-08-09 10:43:47 +0200

    Update: add func to bypass checking for each Animation and customize one

--- HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
+++ HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
@@ -8,20 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    @Environment(\.accessibilityReduceMotion) var reduceMotion
     @State private var scale = 1.0
     
     var body: some View {
         Button("Hello, world!") {
-            if reduceMotion {
+            withOptionalAnimation {
                 scale *= 1.5
-            } else {
-                withAnimation {
-                    scale *= 1.5
-                }
             }
         }
         .scaleEffect(scale)
+    }
+    
+    func withOptionalAnimation<Result>(_ animation: Animation? = .default, _ body: () throws -> Result) rethrows -> Result {
+        if UIAccessibility.isReduceMotionEnabled {
+            return try body()
+        } else {
+            return try withAnimation(animation, body)
+        }
     }
 }
 
