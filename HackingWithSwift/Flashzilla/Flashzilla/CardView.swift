eb4ce89f04def78c65914e82e012b21f4f920a8e
Modzie <modzielol@gmail.com>
2024-08-11 17:19:02 +0200

    Init: CardView file to show a basic card, with a ZStack and VStack with the prompt and the answer, the width of 450 is for the smallest iphone who had a width of 480

---
+++ HackingWithSwift/Flashzilla/Flashzilla/CardView.swift
@@ -0,0 +1,36 @@
+//
+//  CardView.swift
+//  Flashzilla
+//
+//  Created by Thomas Meyer on 11/08/2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(.white)
+            
+            VStack {
+                Text(card.prompt)
+                    .font(.largeTitle)
+                    .foregroundStyle(.black)
+                
+                Text(card.answer)
+                    .font(.title)
+                    .foregroundStyle(.secondary)
+            }
+            .padding(20)
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+    }
+}
+
+#Preview {
+    CardView(card: .example)
+}
