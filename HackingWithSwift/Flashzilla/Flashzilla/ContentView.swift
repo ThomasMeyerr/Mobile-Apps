935bb20d0ab96eab25545c19a8f4110ec2c1152d
Modzie <modzielol@gmail.com>
2024-08-11 17:41:44 +0200

    Add: base for our UI and add a forEach view to display all of our cards using the stacked() modifier we created before

--- HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
+++ HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
@@ -18,7 +18,16 @@ struct ContentView: View {
     @State private var cards = Array<Card>(repeating: .example, count: 10)
     
     var body: some View {
-        CardView(card: .example)
+        ZStack {
+            VStack {
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
     }
 }
 
