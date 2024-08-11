b40504e205331ca276c1b2fb763f74595d4e1dbf
Modzie <modzielol@gmail.com>
2024-08-11 17:39:06 +0200

    Add: extension of View to create a modifier stacked() who offset cards a little further down the screen

--- HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
+++ HackingWithSwift/Flashzilla/Flashzilla/ContentView.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
 struct ContentView: View {
     @State private var cards = Array<Card>(repeating: .example, count: 10)
     
