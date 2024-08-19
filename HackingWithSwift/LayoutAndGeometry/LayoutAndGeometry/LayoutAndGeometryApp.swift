8ac1fa69e66f39a464360ccde822f2aeaae854ba
Modzie <modzielol@gmail.com>
2024-08-19 09:52:00 +0200

    Init: new project

---
+++ HackingWithSwift/LayoutAndGeometry/LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Thomas Meyer on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
