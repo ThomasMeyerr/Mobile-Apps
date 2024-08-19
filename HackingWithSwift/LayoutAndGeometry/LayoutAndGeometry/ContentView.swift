398861aa163949ab89310ac300dce81524364011
Modzie <modzielol@gmail.com>
2024-08-19 11:16:16 +0200

    Add: using position modifier grant a new view which take up all available space that's why the background is full red

--- HackingWithSwift/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ HackingWithSwift/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,34 +7,11 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    struct MidAccountAndName: AlignmentID {
-        static func defaultValue(in context: ViewDimensions) -> CGFloat {
-            context[.top]
-        }
-    }
-    
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
-}
-
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .midAccountAndName) {
-            VStack {
-                Text("@Modzie")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                Image(.example)
-                    .resizable()
-                    .frame(width: 64, height: 64)
-            }
-            
-            VStack {
-                Text("Full name:")
-                Text("THOMAS MEYER")
-                    .alignmentGuide(.midAccountAndName) { d in d[VerticalAlignment.center] }
-                    .font(.largeTitle)
-            }
-        }
+        Text("Hello, world!")
+            .position(x: 100, y: 100)
+            .background(.red)
     }
 }
 
