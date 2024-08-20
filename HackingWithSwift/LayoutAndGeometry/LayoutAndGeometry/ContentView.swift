a632b31e4760ac805eb28749d163a43a24d30eab
Modzie <modzielol@gmail.com>
2024-08-20 11:09:37 +0200

    Add: 3 type of coordinate to show coordinatedepend of parents

--- HackingWithSwift/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ HackingWithSwift/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -7,19 +7,40 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct OuterView: View {
     var body: some View {
         VStack {
-            GeometryReader { proxy in
-                Text("Hello, world!")
-                    .frame(width: proxy.size.width * 0.9, height: 40)
-                    .background(.red)
-            }
-            .background(.green)
-            
-            Text("More text")
-                .background(.blue)
+            Text("Top")
+            InnerView()
+            Text("Bottom")
         }
+    }
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { proxy in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY)")
+                        print("Custom center: \(proxy.frame(in: .named("Custom")).midX) x \(proxy.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
     }
 }
 
