c19e756b27df11d79c3833921ba84c445a671c9c
Modzie <modzielol@gmail.com>
2024-08-20 10:20:56 +0200

    Add: GeometryReader could make a text view have 90% of all available width regardless of its content

--- HackingWithSwift/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ HackingWithSwift/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -9,18 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack {
-            Text("IMPORTANT")
-                .frame(width: 200)
-                .background(.blue)
-            
-            GeometryReader { proxy in
-                Image(.example)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: proxy.size.width * 0.8)
-                    .frame(width: proxy.size.width, height: proxy.size.height)
-            }
+        GeometryReader { proxy in
+            Text("Hello, world!")
+                .frame(width: proxy.size.width * 0.9)
+                .background(.red)
         }
     }
 }
