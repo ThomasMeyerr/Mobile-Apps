5081e5b7492fa9ecf4f1a780bc2fc8e0e9bbb3e7
Modzie <modzielol@gmail.com>
2024-08-20 11:20:58 +0200

    Add: we could use GeometryReader inside another to have a better layout

--- HackingWithSwift/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
+++ HackingWithSwift/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -11,15 +11,17 @@ struct ContentView: View {
     let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
 
     var body: some View {
-        ScrollView {
-            ForEach(0..<50) { index in
-                GeometryReader { proxy in
-                    Text("Row #\(index)")
-                        .font(.title)
-                        .frame(maxWidth: .infinity)
-                        .background(colors[index % 7])
-                        .rotation3DEffect(.degrees(proxy.frame(in: .global).minY / 5), axis: (x: 0, y: 1, z: 0))
-                        .frame(height: 40)
+        GeometryReader { fullView in
+            ScrollView {
+                ForEach(0..<50) { index in
+                    GeometryReader { proxy in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(maxWidth: .infinity)
+                            .background(colors[index % 7])
+                            .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                            .frame(height: 40)
+                    }
                 }
             }
         }
