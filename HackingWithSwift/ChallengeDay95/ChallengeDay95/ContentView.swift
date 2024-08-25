5c4d5580eaf7ff3c908b2350f0532a964562ce47
Modzie <modzielol@gmail.com>
2024-08-25 19:49:34 +0200

    Add: base for selecting dice type and numbers of dices, then calculate the dices

--- HackingWithSwift/ChallengeDay95/ChallengeDay95/ContentView.swift
+++ HackingWithSwift/ChallengeDay95/ChallengeDay95/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var diceNumber = 0
+    @State private var diceType = 0
+    @State private var diceTotal = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            Section("Combien de dés ?") {
+                TextField("Dice number", value: $diceNumber, formatter: NumberFormatter())
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section("Quel type de dé ? 2, 3, 4 faces ou plus ?") {
+                TextField("Quel type de dé ? 2, 3, 4 faces ou plus ?", value: $diceType, formatter: NumberFormatter())
+                    .keyboardType(.decimalPad)
+            }
         }
         .padding()
+        .frame(maxHeight: 250)
+        
+        Button("GO !", action: rollDices)
+            .padding()
+            .background(.blue)
+            .foregroundStyle(.white)
+            .clipShape(.capsule)
+        
+        if diceTotal != 0 {
+            Text(String(diceTotal))
+                .font(.largeTitle)
+        }
+    }
+    
+    func rollDices() {
+        var dices = [Int]()
+        
+        for _ in 0..<diceNumber {
+            dices.append(Int.random(in: 1...diceType))
+        }
+        
+        diceTotal = dices.reduce(0, +)
     }
 }
 
