e692f8ce172f312694d5087551d1121a1dae658d
Modzie <modzielol@gmail.com>
2024-08-25 19:13:26 +0200

    Init: new project

---
+++ HackingWithSwift/ChallengeDay95/ChallengeDay95/ChallengeDay95App.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeDay95App.swift
+//  ChallengeDay95
+//
+//  Created by Thomas Meyer on 25/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeDay95App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackingWithSwift/ChallengeDay95/ChallengeDay95/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChallengeDay95
+//
+//  Created by Thomas Meyer on 25/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
