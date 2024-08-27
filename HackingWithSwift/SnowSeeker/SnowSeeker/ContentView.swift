ff993721896458e9bff7ac5d85411dc73d24d006
Modzie <modzielol@gmail.com>
2024-08-27 11:10:45 +0200

    Add: Once an object has been placed into the environment, any subview can read it back out

--- HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
+++ HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,27 +7,29 @@
 
 import SwiftUI
 
+@Observable
+class Player {
+    var name = "Anonymous"
+    var highScore = 0
+}
+
+struct HighScoreView: View {
+    @Environment(Player.self) var player
+
+    var body: some View {
+        Text("Your highscore is \(player.highScore)")
+    }
+}
 
 struct ContentView: View {
-    @State private var searchText = ""
-    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
-    
-    var filteredNames: [String] {
-        if searchText.isEmpty {
-            allNames
-        } else {
-            allNames.filter { $0.localizedStandardContains(searchText) }
-        }
-    }
+    @State private var player = Player()
     
     var body: some View {
-        NavigationStack {
-            List(filteredNames, id: \.self) { name in
-                Text(name)
-            }
-            .searchable(text: $searchText, prompt: "Look for something")
-            .navigationTitle("Searching")
+        VStack {
+            Text("Welcome")
+            HighScoreView()
         }
+        .environment(player)
     }
 }
 
