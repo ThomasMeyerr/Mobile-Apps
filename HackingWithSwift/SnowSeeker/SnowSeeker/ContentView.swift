0e9a0f63051a7070b5521c10c3c941a89978505a
Modzie <modzielol@gmail.com>
2024-08-27 09:24:31 +0200

    Init: new project to be on ipad

---
+++ HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Thomas Meyer on 27/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, world!")
+                .navigationTitle("Primary")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackingWithSwift/SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Thomas Meyer on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
