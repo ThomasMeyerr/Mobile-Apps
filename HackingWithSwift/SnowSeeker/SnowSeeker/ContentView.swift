d8d68f3a98461aceb078bb7785d40ed88487d2a6
Modzie <modzielol@gmail.com>
2024-08-27 10:30:56 +0200

    Add: iOS can add search bar to our view using the searchable modifier, with a text to bind and a prompt

--- HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
+++ HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,27 +7,15 @@
 
 import SwiftUI
 
-struct UserView: View {
-    var body: some View {
-        Group {
-            Text("Name: Paul")
-            Text("Country: England")
-            Text("Pets: Luna and Arya")
-        }
-        .font(.title)
-    }
-}
 
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    @State private var searchText = ""
     
     var body: some View {
-        ViewThatFits {
-            Rectangle()
-                .frame(width: 500, height: 200)
-            
-            Circle()
-                .frame(width: 200, height: 200)
+        NavigationStack {
+            Text("Searching for \(searchText)")
+                .searchable(text: $searchText, prompt: "Look for something")
+                .navigationTitle("Searching")
         }
     }
 }
