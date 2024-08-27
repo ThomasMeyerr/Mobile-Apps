1acb8ecec6a116ff86c3f8574dee8becf202305e
Modzie <modzielol@gmail.com>
2024-08-27 10:16:09 +0200

    Add: Groups views allow to make a transparent container and apply modifier to each views

--- HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
+++ HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,21 +7,21 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "Taylor Swift"
+struct UserView: View {
+    var body: some View {
+        Group {
+            Text("Name: Paul")
+            Text("Country: England")
+            Text("Pets: Luna and Arya")
+        }
+        .font(.title)
+    }
 }
 
 struct ContentView: View {
-    @State private var selectedUser: User? = nil
     
     var body: some View {
-        Button("Tap Me") {
-            selectedUser = User()
-        }
-        .sheet(item: $selectedUser) { user in
-            Text(user.id)
-                .presentationDetents([.medium, .large])
-        }
+        
     }
 }
 
