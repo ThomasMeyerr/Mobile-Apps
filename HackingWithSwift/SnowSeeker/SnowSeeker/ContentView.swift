4d05e5c1f93b452b863683b21a9a3cf6e06b5efa
Modzie <modzielol@gmail.com>
2024-08-28 09:27:37 +0200

    Add: files and images to project

--- HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
+++ HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,32 +7,14 @@
 
 import SwiftUI
 
-@Observable
-class Player {
-    var name = "Anonymous"
-    var highScore = 0
-}
-
-struct HighScoreView: View {
-    @Environment(Player.self) var player
-
-    var body: some View {
-        @Bindable var player = player
-        Stepper("High score: \(player.highScore)", value: $player.highScore)
-    }
-}
 
 struct ContentView: View {
-    @State private var player = Player()
     
     var body: some View {
-        VStack {
-            Text("Welcome")
-            HighScoreView()
-        }
-        .environment(player)
+        Text("Hello, world!")
     }
 }
+
 
 #Preview {
     ContentView()
