99816142f78f1bcdeb7039630e67984f75844bbe
Modzie <modzielol@gmail.com>
2024-08-31 17:34:50 +0200

    Add: enum to sort 3 types of order and adapt the code

--- HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
+++ HackingWithSwift/SnowSeeker/SnowSeeker/ContentView.swift
@@ -7,12 +7,22 @@
 
 import SwiftUI
 
+enum Filter {
+    case defaults, alphabetical, country
+}
+
 
 struct ContentView: View {
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
     var filteredResorts: [Resort] {
         if searchText.isEmpty {
-            resorts
+            if filter == .alphabetical {
+                resorts.sorted { $0.name < $1.name }
+            } else if filter == .country {
+                resorts.sorted { $0.country < $1.country }
+            } else {
+                resorts
+            }
         } else {
             resorts.filter { $0.name.localizedStandardContains(searchText) }
         }
@@ -20,6 +30,7 @@ struct ContentView: View {
     
     @State private var searchText = ""
     @State private var favorites = Favorites()
+    @State private var filter: Filter = .defaults
     
     var body: some View {
         NavigationSplitView {
@@ -59,6 +70,19 @@ struct ContentView: View {
                 ResortView(resort: resort)
             }
             .searchable(text: $searchText, prompt: "Search for a resort")
+            .toolbar {
+                ToolbarItem {
+                    Button("Default") { filter = .defaults }
+                }
+                
+                ToolbarItem {
+                    Button("Alphabetical") { filter = .alphabetical }
+                }
+                
+                ToolbarItem {
+                    Button("Country") { filter = .country }
+                }
+            }
         } detail: {
             WelcomeView()
         }
