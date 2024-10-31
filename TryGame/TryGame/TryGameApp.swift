bc73f2f0f4421b332920778c95215d6c7e3d6f23
Modzie <modzielol@gmail.com>
2024-10-31 17:01:26 +0100

    init: new project

---
+++ TryGame/TryGame/TryGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryGameApp.swift
+//  TryGame
+//
+//  Created by Thomas Meyer on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TryGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
