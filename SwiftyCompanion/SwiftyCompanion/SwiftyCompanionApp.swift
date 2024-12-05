c29a71867394e504062c13c78df14c7b6349555d
Modzie <modzielol@gmail.com>
2024-12-05 17:45:43 +0100

    Init: new project with image and subject pdf

---
+++ SwiftyCompanion/SwiftyCompanion/SwiftyCompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftyCompanionApp.swift
+//  SwiftyCompanion
+//
+//  Created by Thomas Meyer on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftyCompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
