90c0c325cd0e47e18b8a64e170ed31dba403003b
Modzie <modzielol@gmail.com>
2024-12-19 16:50:48 +0100

    Add: base for download api info with unique error

--- SwiftyCompanion/SwiftyCompanion/42View.swift
+++ SwiftyCompanion/SwiftyCompanion/42View.swift
@@ -7,9 +7,43 @@
 
 import SwiftUI
 
+
+enum NetworkError: Error {
+    case badUrl
+    case invalidRequest
+    case badResponse
+    case badStatus
+    case failedToDecodeResponse
+}
+
+
 struct _42View: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    
+    func downloadData<T: Codable>(fromUrl: String) async -> T? {
+        do {
+            guard let url = URL(string: fromUrl) else { throw NetworkError.badUrl }
+            let (data, response) = try await URLSession.shared.data(from: url)
+            guard let response = response as? HTTPURLResponse else { throw NetworkError.badResponse }
+            guard response.statusCode >= 200 && response.statusCode < 300 else { throw NetworkError.badStatus }
+            guard let decodedResponse = try? JSONDecoder().decode(T.self, from: data) else { throw NetworkError.failedToDecodeResponse }
+        } catch NetworkError.badUrl {
+            //
+        } catch NetworkError.invalidRequest {
+            //
+        } catch NetworkError.badResponse {
+            //
+        } catch NetworkError.badStatus {
+            //
+        } catch NetworkError.failedToDecodeResponse {
+            //
+        } catch {
+            //
+        }
+            
+        return nil
     }
 }
 
