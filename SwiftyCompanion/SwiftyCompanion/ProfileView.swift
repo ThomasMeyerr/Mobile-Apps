dbe8f3496636f2b33640dcef20cf6ddc555e1502
Modzie <modzielol@gmail.com>
2025-01-09 17:12:15 +0100

    WIP: define layout of ProfileView

--- SwiftyCompanion/SwiftyCompanion/ProfileView.swift
+++ SwiftyCompanion/SwiftyCompanion/ProfileView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 
+@MainActor class ProfileViewModel: ObservableObject {
+    @Published var isAlert = false
+}
+
+
 struct ProfileView: View {
-    @StateObject var contentVM: ContentViewModel
+    @ObservedObject var contentVM: ContentViewModel
+    @StateObject var vm = ProfileViewModel()
+    
     let example = User(id: 1, email: "omg@gmail.com", login: "thmeyer", firstName: "Thomas", lastName: "Meyer", image: UserImage(link: "www.caexistepas.com"), correctionPoint: 3, wallet: 1064, location: nil, active: true)
     
     init(contentVM: ContentViewModel) {
-        self._contentVM = StateObject(wrappedValue: contentVM)
+        self._contentVM = ObservedObject(wrappedValue: contentVM)
     }
     
     var body: some View {
@@ -21,34 +28,48 @@ struct ProfileView: View {
             AsyncImage(url: URL(string: example.image.link)) { image in
                 image
                     .resizable()
-                    .aspectRatio(contentMode: .fill)
+                    .scaledToFill()
+                    .clipShape(Circle())
             } placeholder: {
                 ProgressView()
                     .scaleEffect(5)
                     .tint(.white)
             }
             .frame(width: 250, height: 250)
+            .background(
+                Image("Water")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea()
+            )
             
-            Form {
-                Section {
-                    Text(example.login)
-                }
+            HStack {
+                Text("₳ \(example.wallet)")
                 
-                Section {
-                    Button("Sign out") {
-                        contentVM.isLogged = false
-                        contentVM.isSheet = true
-                    }
-                    .foregroundStyle(.red)
+                Text(example.login)
+                    .bold()
+                
+                Group {
+                    Text(example.firstName)
+                    Text(example.lastName)
                 }
+                .foregroundStyle(.secondary)
+            }
+                
+            Button {
+                vm.isAlert = true
+            } label: {
+                Image(systemName: "person.crop.circle.badge.xmark.fill")
+                    .tint(.red)
             }
         }
-        .background(
-            Image("Water")
-                .resizable()
-                .scaledToFill()
-                .ignoresSafeArea()
-        )
+        .alert("Are you sure?", isPresented: $vm.isAlert) {
+            Button("Yes") {
+                contentVM.isLogged = false
+                contentVM.isSheet = true
+            }
+            Button("No", role: .cancel) {}
+        }
     }
 }
 
