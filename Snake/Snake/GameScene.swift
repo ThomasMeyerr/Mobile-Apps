74340629a2ab228e159adf25ef350dea3d4a7067
Modzie <modzielol@gmail.com>
2025-01-21 16:44:58 +0100

    Init: GameScene with SpriteKit, with 2 override func

---
+++ Snake/Snake/GameScene.swift
@@ -0,0 +1,27 @@
+//
+//  GameScene.swift
+//  Snake
+//
+//  Created by Thomas Meyer on 21/01/2025.
+//
+
+import SpriteKit
+
+
+class GameScene: SKScene {
+    var snake: SKSpriteNode!
+    
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+        
+        // Create a basic snake
+        snake = SKSpriteNode(color: .green, size: CGSize(width: 20, height: 20))
+        snake.position = CGPoint(x: frame.midX, y: frame.midY)
+        addChild(snake)
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Logicial to move the snake (with touch?)
+        snake.position.x += 1
+    }
+}
