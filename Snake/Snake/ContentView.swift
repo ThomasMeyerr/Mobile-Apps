2d8bb7566bffcfbd11e8c3bb1da96acea1c63de2
Modzie <modzielol@gmail.com>
2025-01-21 15:55:23 +0100

    Init: new project

---
+++ Snake/Snake/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Snake
+//
+//  Created by Thomas Meyer on 21/01/2025.
+//
+
+import SwiftUI
+
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Snake/Snake/SnakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnakeApp.swift
+//  Snake
+//
+//  Created by Thomas Meyer on 21/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct SnakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
