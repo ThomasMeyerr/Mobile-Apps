5ab11e33a99f9c77282f657a6e11f4ef00854577
Modzie <modzielol@gmail.com>
2025-01-22 09:17:45 +0100

    Init: new project to test SceneKit

---
+++ Snake3D/Snake3D/Snake3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Snake3DApp.swift
+//  Snake3D
+//
+//  Created by Thomas Meyer on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Snake3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
