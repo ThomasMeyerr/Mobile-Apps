05d4729e97a2398f631100103dbfae682e44a9c9
Modzie <modzielol@gmail.com>
2025-02-19 16:03:09 +0100

    Init: project in react native to test

---
+++ testReactNative/pokenative/app/index.tsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+
+export default function Index() {
+	return (
+		<View style={styles.container}>
+			<Text>Edit app/index.tsx to edit this screen.</Text>
+		</View>
+	);
+}
+
+
+const styles = {
+	container: {
+		backgroundColor: '#FF0000',
+		padding: 24,
+	}
+}
