8b226026212829cfb45738f4da71847fe3be71f1
Modzie <modzielol@gmail.com>
2025-02-19 16:44:30 +0100

    Init: color.ts file to store all colors depending of light and darkmode

---
+++ testReactNative/pokenative/constants/Colors.ts
@@ -0,0 +1,18 @@
+export const Colors = {
+	light: {
+		tint: "#DC0A2D",
+		grayDark: "#212121",
+		grayMedium: "#666666",
+		grayLight: "#E0E0E0",
+		grayBackground: "#EFEFEF",
+		grayWhite: "#FFFFFF",
+	},
+	dark: {
+		tint: "#DC0A2D",
+		grayDark: "#212121",
+		grayMedium: "#666666",
+		grayLight: "#E0E0E0",
+		grayBackground: "#EFEFEF",
+		grayWhite: "#FFFFFF",
+	}
+};
